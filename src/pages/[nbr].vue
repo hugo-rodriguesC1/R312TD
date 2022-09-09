e7fd6bde24f2758834a1bb2423a54bfc587c31dd
hugo-rodriguesC1 <hugo.rodrigues@edu.univ-fcomte.fr>
2022-09-09 09:53:08 +0200

    routes dynamiques

---
+++ src/pages/[nbr].vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from "../components/card.vue";
+import maisons from "../assets/maisons.json";
+
+defineProps({ nbr: String });
+</script>
+            
+        <template>
+            <div>
+                <div class="p-2">Page liste avec routes dynamiques</div>
+            </div>
+                <Card v-bind="maisons[nbr]"/>
+
+    
+    
+        </template>
+        
+        
