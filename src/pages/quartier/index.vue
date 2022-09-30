f989462528273df9a665d7231eb496235938881c
hugo-rodriguesC1 <hugo.rodrigues@edu.univ-fcomte.fr>
2022-09-30 08:20:44 +0200

    disclosure

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { supabase } from "../../supabase";
+import groupBy from "lodash/groupBy";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+
 const { data, error } = await supabase.from("allquartier").select("*");
 if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
 </script>
@@ -13,5 +16,17 @@ if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
             {{ quartierObject.quartier_nom }}
           </li>
         </ul>
+        <br>
+
+        <h3>Disclosure</h3>
+        <Disclosure v-for="(listeQuartier, Commune_nom) in groupBy(data,'commune_nom')" :key="Commune_nom">
+            <DisclosureButton>{{Commune_nom}}</DisclosureButton>
+            <DisclosurePanel>
+                <li
+                    v-for="quartierObject in listeQuartier"
+                    :key="quartierObject.code_Quartier"
+                >{{quartierObject.quartier_nom}}</li>
+            </DisclosurePanel>
+        </Disclosure>
       </section>
     </template>
