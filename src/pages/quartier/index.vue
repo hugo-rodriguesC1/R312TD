abfa53a3fddcce464dd30a6e17bd7d88dbb492b5
hugo-rodriguesC1 <hugo.rodrigues@edu.univ-fcomte.fr>
2022-09-30 08:11:52 +0200

    liste simple sur quartiercommune

---
+++ src/pages/quartier/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { supabase } from "../../supabase";
+const { data, error } = await supabase.from("allquartier").select("*");
+if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
+</script>
+    
+    <template>
+      <section class="flex flex-col">
+        <h3 class="text-2xl">Liste des quartiers</h3>
+        <ul>
+          <li v-for="quartierObject in (data)" :key="quartierObject.id">
+            {{ quartierObject.commune_nom }} -
+            {{ quartierObject.quartier_nom }}
+          </li>
+        </ul>
+      </section>
+    </template>
